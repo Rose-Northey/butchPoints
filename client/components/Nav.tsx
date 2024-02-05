af04a3b57f6a71983494107ecce95e86ebfa75cc
rose northey <roseAnorthey@gmail.com>
2024-02-05 18:01:14 +1300

    connect signin and signout button to authenticated status

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom"
+import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated"
 
 export default function Nav() {
+
+
+  const handleSignOut=()=>{
+    console.log('sign out')
+  }
+
+  const handleSignIn=()=>{
+    console.log('sign in')
+  }
+
 
 
   return (
   <>
   <nav className= 'hflex'>
-    <button>
-      Log In
-    </button>
+    <IfAuthenticated>
+      <button>
+        Log Out
+      </button>
+    </IfAuthenticated>
+    <IfNotAuthenticated>
+      <button>
+        Log In
+      </button>
+    </IfNotAuthenticated>
+
     <button>
       Butch Book
     </button>
