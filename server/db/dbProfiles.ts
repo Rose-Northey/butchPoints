72867c51d16d8070aca812da0300b8121718ee4d
rose northey <roseAnorthey@gmail.com>
2024-02-12 11:59:36 +1300

    add profileRoutes

--- server/db/dbProfiles.ts
+++ server/db/dbProfiles.ts
@@ -3,15 +3,6 @@ import { NewProfile } from "../../models/modelsProfiles"
 import { convertToSnakeCase } from "../universalFunctions"}
 
 
-// export interface NewProfile{
-//   userToken: string
-//   firstName: string
-//   lastName: string
-//   emailAddress: string
-// }
-
-// convert cammelCase to Snake Case
-
 export async function addProfile(newProfile:NewProfile, db = connection){
   try{
     db('profiles').insert({
---
+++ server/routes/profileRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+
+import * as db from '../db/dbProfiles'
+
+const router = Router()
+
+router.get('/add', async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
+})
+
+export default router
