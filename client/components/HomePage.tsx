fec455b451ecd83d2f5cd3eae6d62731e7559abc
rose northey <roseAnorthey@gmail.com>
2024-02-12 11:11:09 +1300

    convert to snakecase function made

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -2,9 +2,8 @@ import { useState} from "react"
 import Profile from "./Profile"
 import Form from "./Form"
 import PointsLog from "./PointsLog"
+import { convertToSnakeCase } from "../../server/universalFunctions"
 export default function HomePage() {
-
-
 
 
   return (
--- server/db/dbProfiles.ts
+++ server/db/dbProfiles.ts
@@ -1,6 +1,7 @@
 import connection from "./connection"
 import { NewProfile } from "../../models/modelsProfiles"
 
+
 // export interface NewProfile{
 //   userToken: string
 //   firstName: string
@@ -8,12 +9,13 @@ import { NewProfile } from "../../models/modelsProfiles"
 //   emailAddress: string
 // }
 
-// convert 
-
+// convert cammelCase to Snake Case
+console.log(convertToSnakeCase("happyDays"))
+const hey = convertToSnakeCase("happyDays")
+console.log("anything")
 export async function addProfile(newProfile:NewProfile, db = connection){
   
 
   return db('profiles')
   .insert(newProfile)
-  .returning 
 }
