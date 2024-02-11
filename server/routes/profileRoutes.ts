04feeae875580448e0b6b32324cd7ddf1d0130a9
rose northey <roseAnorthey@gmail.com>
2024-02-12 12:10:23 +1300

    add new artwork backend server router

--- server/routes/profileRoutes.ts
+++ server/routes/profileRoutes.ts
@@ -4,11 +4,15 @@ import * as db from '../db/dbProfiles'
 
 const router = Router()
 
-router.get('/add', async (req, res) => {
+
+// /api/v1/profiles/newUser
+router.post('/newUser', async (req, res) => {
   try {
-
+    const newProfile = req.body
+    const newUserId = await db.addProfile(newProfile)
+    res.status(200).json(newUserId)
   } catch (error) {
-
+    console.error('error at server router', error)
   }
 })
 
