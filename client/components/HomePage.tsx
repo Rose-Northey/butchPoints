8614d90501333982a8e5ec11945c3b672eeb8522
rose northey <roseAnorthey@gmail.com>
2024-02-12 13:39:21 +1300

    addProfile api function

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -2,9 +2,10 @@ import { useState} from "react"
 import Profile from "./Profile"
 import Form from "./Form"
 import PointsLog from "./PointsLog"
+import { addProfile } from "../apis/profile"
 import { convertToSnakeCase } from "../../server/universalFunctions"
 export default function HomePage() {
-
+  addProfile()
 
   return (
   <>
