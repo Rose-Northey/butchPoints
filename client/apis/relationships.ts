235f21ed253eff99c66c2fb1190075489be44c56
student <student@pop-os.localdomain>
2024-02-15 11:42:53 +1300

    attempting to fix query

--- client/apis/relationships.ts
+++ client/apis/relationships.ts
@@ -1,8 +1,8 @@
 import request from 'superagent'
 
-export async function getRequestsForUser(userEmail:string){
+export async function getRequestsForUser(userEmail:string|undefined){
   const bottomEmail = {bottomEmail:userEmail}
   const requests = await request.post('/api/v1/relationships').send(bottomEmail)
-  console.log('requests', requests.body)
+  // console.log('requests', requests.body)
   return requests.body
 }
