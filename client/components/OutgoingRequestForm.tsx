88b0d3669da2535701f8286ef6c52662e2ef5381
rose northey <roseAnorthey@gmail.com>
2024-02-29 15:41:28 +1300

    create form in OUtgoingRequest

--- client/components/OutgoingRequestForm.tsx
+++ client/components/OutgoingRequestForm.tsx
@@ -2,6 +2,20 @@ export default function OutgoingRequestsForm(){
   
   return(
 <>
+<h3>Outgoing Requests</h3>
+<form>
+  <label>
+    Type your lover's email to request a relationship (so romantic)
+  <input
+    type="email"
+    name="email"
+    value={email}
+    onChange={handleEmailChange}
+  />
+  </label>
+  <br/>
+  <button type="submit" onClick={submitRequest}>GRAND ROMANTIC GESTURE AWAY!!</button>
+</form>
 
 </>
   )
