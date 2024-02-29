cd3393bea39792e4574f2079ed87d3fbf8c47115
rose northey <roseAnorthey@gmail.com>
2024-02-29 16:35:04 +1300

    add auth0 to api relationship file

--- client/apis/relationships.ts
+++ client/apis/relationships.ts
@@ -1,3 +1,11 @@
+import {useAuth0} from '@auth0/auth0-react'
+
+const {user, getAccessTokenSilently} = useAuth0()
+const topName= user?.given_name
+const emailAddress= user?.email
+
+
+
 export function submitRequest(email:string){
   console.log(email)
 }
