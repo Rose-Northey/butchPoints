4bb0bbf22957f545f4547dbcc6b70c02cf17f660
rose northey <roseAnorthey@gmail.com>
2024-03-06 16:24:29 +1300

    "create backend route for post gettin relationshi"

--- server/routes/relationshipRoutes.ts
+++ server/routes/relationshipRoutes.ts
@@ -15,4 +15,14 @@ router.post('/new', async (req, res) => {
   }
 })
 
+// /api/v1/relationships/new
+router.post('/', async (req, res) =>{
+  try{
+    const userEmail = req.body
+    res.status(200).json(incomingRequests)
+  }catch(error){
+    console.error('error at server router for getting relationship requests',error)
+  }
+})
+
 export default router
